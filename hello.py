4a9410b2aa2309622690de340bb94ca072f63192
Caitlinlaplante <caitlaplante@icloud.com>
2026-01-23 16:33:16 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,35 +1,35 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+#Caitlin Laplante 101187525
 #Part 5
 #Part 5.1
 t = np.arange(0, 1, 1/500)
-data = 5*np.sin(2*np.pi*10*t)
+data = 5*np.sin(2*np.pi*10*t) #sine function
 
 
 plt.plot(t,data)
 plt.xlabel("Time(s)")
 plt.ylabel("Amplitude")
-plt.show()
+plt.show() #plot the signal
 
 #Part 5.2
 t = np.arange(0, 1, 1/500)
-data = 5*np.sin(2*np.pi*10*t)
-noise = 0.25*np.random.randn(len(t))
-signal = data + noise
+data = 5*np.sin(2*np.pi*10*t) #sine function
+noise = 0.25*np.random.randn(len(t)) #noise function
+signal = data + noise #signal with noise
 
 
 plt.plot(t,signal)
 plt.xlabel("Time(s)")
 plt.ylabel("Amplitude")
-plt.show()
+plt.show() #plot the signal
 
 #Part 5.3
 t = np.arange(0,120*60, 60)
-temp = 36.0 + np.random.uniform(-0.5, 0.5, 120)
+temp = 36.0 + np.random.uniform(-0.5, 0.5, 120) #uniform temperature signal
 
-df = pd.DataFrame({"Time (s)": t, "Temperature (°C)": temp})
+df = pd.DataFrame({"Time (s)": t, "Temperature (°C)": temp}) #Creating title for csv file
 
 # Save to CSV
 df.to_csv("sensor_readings.csv", index = False)
@@ -49,29 +49,31 @@ plt.show()
 
 #Part 6
 #6.4.2
-df = pd.read_csv("env_temp_humidity_clean.csv")
-print("First 5 rows:")
+df = pd.read_csv("env_temp_humidity_clean.csv") #reads csv file
+print("First 5 rows:") #takes the first 5 rows
 print(df.head())
 
-print("\nData types:")
+print("\nData types:")#shows columns data types
 print(df.columns)
 
-print("\nData types:")
+print("\nData types:")#shows data types
 print(df.dtypes)
 
 #6.4.3
-print("\nMissing values per column:")
+print("\nMissing values per column:") #shows any missing values
 print(df.isnull().sum())
 
-print("\nMin / Max values:")
+print("\nMin / Max values:") #shows the min and max of the values in each columns
 print(df[["temperature_C", "humidity_pct"]].agg(["min", "max"]))
 
+#Plots time vs. temperature
 plt.figure()
 plt.plot(df["timestamp"],df["temperature_C"])
 plt.xlabel("Time")
 plt.ylabel("Temperature (°C)")
 plt.show()
 
+#Plots time vs. humidity
 plt.figure()
 plt.plot(df["timestamp"],df["humidity_pct"])
 plt.xlabel("Time")
